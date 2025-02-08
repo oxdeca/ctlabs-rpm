49118c347de4ac50b1f5af5f81f6bebebcc56a93
oxdeca <acedxo@oxdeca.com>
2025-02-08 16:13:50 -0500

    termshark build

---
+++ ctlabs_termshark/build.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+# ------------------------------------------------------------------------------
+# SOURCES
+# ------------------------------------------------------------------------------
+NAME=termshark
+VERSION=2.4.0
+SRC_TAR=${NAME}_${VERSION}_linux_x64.tar.gz
+SRC_URL=https://github.com/gcla/termshark/releases/download/v${VERSION}/${SRC_TAR}
+
+# ------------------------------------------------------------------------------
+# Package Attributes
+# ------------------------------------------------------------------------------
+DESC="A terminal user-interface for tshark"
+URL=https://github.com/gcla/termshark/
+PKG_NAME=ctlabs_${NAME}-${VERSION}
+RPM_NAME=${PKG_NAME}.rpm
+DEPS=( 'wireshark-cli' )
+
+# ------------------------------------------------------------------------------
+# Build Details
+# ------------------------------------------------------------------------------
+BINARY=${NAME}
+SRCDIR=${NAME}_${VERSION}_linux_x64
+DSTDIR=/opt/${NAME}-${VERSION}
+
+# ------------------------------------------------------------------------------
+# Build Functions
+# ------------------------------------------------------------------------------
+prepare() {
+  mkdir /tmp/${NAME}
+  cd /tmp/${NAME} && curl -sLO ${SRC_URL} && tar xvf ${SRC_TAR}
+  cd -
+}
+
+build() {
+  #cd /tmp/${NAME}/${SRCDIR}   &&
+  ln -sv ${DSTDIR}/${BINARY} /tmp/${NAME}/${SRCDIR}/${NAME}_LINK
+
+  fpm -s dir                  \
+      -t rpm                  \
+      -p ${RPM_NAME}          \
+      -n ${PKG_NAME}          \
+      -v ${VERSION}           \
+      -a all                  \
+      -d wireshark-cli        \
+      --url ${URL}            \
+      --description "${DESC}" \
+      /tmp/${NAME}/${SRCDIR}/${BINARY}=${DSTDIR}/${BINARY} \
+      /tmp/${NAME}/${SRCDIR}/${NAME}_LINK=/usr/bin/${BINARY}
+}
+
+cleanup() {
+  rm -rf /tmp/${NAME}
+}
+
+prepare
+build
+#cleanup
